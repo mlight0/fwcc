a808619ed613d58c1408f0f91d167a50117826b2
mlight0 <michael.light1@gmail.com>
2017-09-06 16:53:33 -0400

    Initial upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<style type="text/css">
+    
+		body {font-family: arial; font-size: 10px; margin: 20px;}
+        .homepagebackground { background: #0b3857 url("images/screen-background.jpg") no-repeat top; background-size: 100%;}
+        .jumbotron {background-color: #467b99; color: #ffffff;}
+        .spacer {font-size: 5px;}
+        .terms {font-size: 12px;}
+        .terms a {color: #bee7fe;}
+        
+	</style>
+</head>
+<body class="homepagebackground">
+
+<div class="container-fluid">
+
+<div class="row">
+  <div class="col-sm-3">&nbsp;</div>
+  <div class="col-sm-6">
+
+<div class="jumbotron text-center">
+  <img src="images/fwcc-logo.png"  class="img-responsive" />
+  <h1>Free Wifi</h1>
+  <br/>
+  <div class="text-left">
+    <p>
+        Thank you for visiting Family Worship Center Church! As our guest, 
+        we are pleased to offer free wireless internet access.
+    </p> 
+    <p>
+        Enter your email address 
+        below and click "Accept and Connect".
+    </p>
+  </div>
+  <div class="spacer">&nbsp;</div>
+  <form class="form-group">
+    <div>
+        <input type="email" class="form-control" placeholder="Email Address" required>
+    </div>
+    <div class="spacer">&nbsp;</div>
+    <div>
+        <button type="submit" class="btn btn-success btn-md btn-block">Accept and Connect</button>
+       <br/>
+        <div class="terms">
+            I agree to the <a href="#">Terms of Service</a>.
+        </div>
+    </div>
+  </form>
+</div>
+
+</div>
+  <div class="col-sm-3">&nbsp;</div>
+</div>
+
+</body>
+</html>
